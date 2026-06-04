97487a745a877e7b2d4208987ee88776e83b9f3f
FiguringToCode <marineryash2000@gmail.com>
2026-06-04 14:03:22 +0530

    .

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-require('dotenv').config()
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+const dotenv = require('dotenv')
+
+('dotenv').config()
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
